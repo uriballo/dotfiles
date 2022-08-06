16eab59cae55bd74c20ef67c3be9d230d990bf0b
Uri <56364336+uriballo@users.noreply.github.com>
2022-08-06 15:29:22 +0200

    Initial commit

---
+++ nvim/lua/options.lua
@@ -0,0 +1,29 @@
+local o = vim.o
+local g = vim.g
+local fn = vim.fn
+
+o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50" -- block in normal and beam cursor in insert mode
+o.updatetime = 300 -- faster completion
+
+o.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+o.termguicolors = true
+o.scrolloff = 8
+
+o.number = true
+o.numberwidth = 2
+o.relativenumber = false
+
+o.expandtab = true
+o.smarttab = true
+o.cindent = true
+o.autoindent = true
+o.wrap = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = -1
+o.list = true
+o.clipboard = 'unnamedplus'
+o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂'
+
+g.mapleader = ' '
+g.maplocalleader = ' '
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,53 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+-- bootstrap packer if not installed
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({
+    "git",
+    "clone",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer...")
+  vim.api.nvim_command("packadd packer.nvim")
+end
+
+-- initialize and configure packer
+local packer = require("packer")
+
+packer.init({
+  enable = true, -- enable profiling via :PackerCompile profile=true
+  threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+  max_jobs = 20, -- Limit the number of simultaneous jobs. nil means no limit. Set to 20 in order to prevent PackerSync form being "stuck" -> https://github.com/wbthomason/packer.nvim/issues/746
+  -- Have packer use a popup window
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+packer.startup(function(use)
+  use("wbthomason/packer.nvim")
+
+  use({
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "f3fora/cmp-spell",
+      "hrsh7th/cmp-calc",
+      "lukas-reineke/cmp-rg",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+  })
+
+  use({
+    "L3MON4D3/LuaSnip",
+    requires = "saadparwaiz1/cmp_luasnip",
+  })
+end)
