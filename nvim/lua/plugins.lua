3616063e5c0b14b2f1cf6a1d8c18ac1bd81cb263
Uri <56364336+uriballo@users.noreply.github.com>
2022-08-07 12:07:52 +0200

    Refactor

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,89 +1,72 @@
 local fn = vim.fn
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
--- bootstrap packer if not installed
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({
+  PACKER_BOOTSTRAP = fn.system {
     "git",
     "clone",
+    "--depth",
+    "1",
     "https://github.com/wbthomason/packer.nvim",
     install_path,
-  })
-  print("Installing packer...")
-  vim.api.nvim_command("packadd packer.nvim")
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
--- initialize and configure packer
-local packer = require("packer")
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
 
-packer.init({
-  enable = true, -- enable profiling via :PackerCompile profile=true
-  threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
-  max_jobs = 20, -- Limit the number of simultaneous jobs. nil means no limit. Set to 20 in order to prevent PackerSync form being "stuck" -> https://github.com/wbthomason/packer.nvim/issues/746
-  -- Have packer use a popup window
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
   display = {
     open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
+      return require("packer.util").float { border = "rounded" }
     end,
   },
-})
+}
 
-packer.startup(function(use)
-  use("wbthomason/packer.nvim")
-
-  use 'neovim/nvim-lspconfig'
-
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "f3fora/cmp-spell",
-      "hrsh7th/cmp-calc",
-      "lukas-reineke/cmp-rg",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-    },
-  })
-
-  use({
-    "L3MON4D3/LuaSnip",
-    requires = "saadparwaiz1/cmp_luasnip",
-  })
-
-  use("lervag/vimtex")
-
+-- Install your plugins here
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "nvim-lua-line/lualine.nvim"
   use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
+	  "windwp/nvim-autopairs",
+      config = function() require("nvim-autopairs").setup {} end
   }
-
   use {
-      'goolord/alpha-nvim',
+      "goolord/alpha-nvim",
       config = function ()
-      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+          require'alpha'.setup(require'alpha.themes.dashboard'.config)
       end
   }
-use {
-'kyazdani42/nvim-tree.lua',
-requires = 'kyazdani42/nvim-web-devicons'
-}
--- bufferline
-use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
   use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
+      "kyazdani42/nvim-tree.lua",
+      requires = "kyazdani42/nvim-web-devicons"
   }
-  use({
-    "olimorris/onedarkpro.nvim",
-    config = function()
-      require("onedarkpro").setup()
-    end
-  })
   use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+      "olimorris/onedarkpro.nvim",
+      config = function()
+          require("onedarkpro").setup()
+      end
   }
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
