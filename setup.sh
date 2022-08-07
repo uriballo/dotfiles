0be835e81c2c30e18b90c94f580d36ec5596bfe1
Miquel de Domingo <miquelddg@gmail.com>
2022-08-07 18:24:00 +0200

    feat!: setup configuration for nvchad

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env zsh
+
+# Mandatori packages
+[ ! -x "$(command -v stow)" ] && echo "ERROR: stow is not installed in the system" && exit -1
+
+NVIM_CONFIG="$HOME/.config/nvim"
+
+# Clone nvchad
+[ ! -d "$NVIM_CONFIG" ] && git_clone NvChad/NvChad "$NVIM_CONFIG" --depth 1
+
+# Stow the custom folder of nvchad
+stow -vR nvim
+
